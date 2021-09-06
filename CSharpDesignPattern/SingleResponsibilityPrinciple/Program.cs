973ecd81e173f646e1f8f2114855de1543bb6f92
HAIPT-PC\HaiPT-PC <haipt4789@gmail.com>
2021-09-06 08:25:38 +0700

    add SOLID sample

---
+++ CSharpDesignPattern/SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, journal.ToString());
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var journal = new Journal();
+            journal.AddEntry("I cried today.");
+            journal.AddEntry("I ate a bug.");
+            Console.WriteLine(journal);
+
+            var p = new Persistence();
+            var filename = @"C:\Temp\journal.txt";
+            p.SaveToFile(journal, filename);
+        }
+    }
+}
